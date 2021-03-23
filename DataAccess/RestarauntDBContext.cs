e4076b0f69c73a945fb5fd40e72e08a184735812
Madeline <madeline.harrop@gmail.com>
2021-03-23 14:26:20 +0000

    Maddy-Liveshare

--- DataAccess/RestarauntDBContext.cs
+++ DataAccess/RestarauntDBContext.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using CitiesAPI;
+using CitiesAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CitiesAPI.DataAccess
 {
-    public class RestarauntDBContext
+    public class RestarauntDBContext : DbContext
     {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Restaraunt> Restaurants { get; set; }
         
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
